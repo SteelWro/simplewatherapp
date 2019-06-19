fb119fb717aaab3eab34fbe6674a6918b407ea98
SteelWro <cwiklinski.artur@wp.pl>
2019-06-19 14:51:41 +0200

    Writing shceduled weather to file

--- src/main/java/com/example/simplewatherapp/ScheduledWeather.java
+++ src/main/java/com/example/simplewatherapp/ScheduledWeather.java
@@ -4,32 +4,82 @@ import com.example.simplewatherapp.model.CurrentWeather;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.net.URISyntaxException;
+import java.io.*;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 @Component
 public class ScheduledWeather {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledWeather.class);
-    private long iterator;
+
+    @Value("${file.path}")
+    String path;
 
     WeatherService weatherService;
     CurrentWeather currentWeather;
-    LocalDateTime localDateTime;
+    LocalDateTime sunrise, sunset;
+    Writer output = null;
+    File file = new File(path);
 
     @Autowired
     public ScheduledWeather(WeatherService weatherService) {
         this.weatherService = weatherService;
-        iterator = 1L;
+        sunrise = null;
+        sunset = null;
     }
 
-    @Scheduled(fixedDelay = 10000)
-    public void printWeather() throws URISyntaxException {
+    @EventListener(ContextRefreshedEvent.class)
+    public void firstLineWriter() {
         currentWeather = weatherService.getCurrentWeather();
-        String WeatherLog = String.format("%s %s",currentWeather.getName(),currentWeather.getMain().getTemp());
-        log.info(WeatherLog);
+        long sunriseUNIX = currentWeather.getSys().getSunrise();
+        long sunsetUNIX = currentWeather.getSys().getSunset();
+
+        sunrise = LocalDateTime
+                .ofInstant(Instant.ofEpochMilli(sunriseUNIX * 1000), TimeZone.getTimeZone("7200").toZoneId());
+        sunset = LocalDateTime
+                .ofInstant(Instant.ofEpochMilli(sunsetUNIX * 1000), TimeZone.getTimeZone("7200").toZoneId());
+
+        String firstWeatherLog = String.format("%s %s", sunrise, sunset);
+
+        try {
+            output = new BufferedWriter(new FileWriter(file, false));
+            output.write(String.format("%s %s %s \n", currentWeather.getName(), sunrise.toLocalTime(), sunset.toLocalTime()));
+            output.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        log.info(firstWeatherLog);
+    }
+
+    @Scheduled(fixedDelay = 60000)
+    public void scheduledWeatherWriter() {
+        currentWeather = weatherService.getCurrentWeather();
+        String weatherLog = String.format("%s %s %s %s %s \n",
+                LocalDateTime.now(),
+                currentWeather.getMain().getTemp(),
+                currentWeather.getMain().getHumidity(),
+                currentWeather.getMain().getPressure(),
+                currentWeather.getWind().getSpeed());
+
+        try {
+            output = new BufferedWriter(new FileWriter(file, true));
+            output.write(weatherLog);
+            output.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        log.info(weatherLog);
     }
 }
--- src/main/java/com/example/simplewatherapp/Starter.java
+++ src/main/java/com/example/simplewatherapp/Starter.java
@@ -1,46 +1,23 @@
 package com.example.simplewatherapp;
 
 import com.example.simplewatherapp.model.CurrentWeather;
+import org.apache.tomcat.jni.Time;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.TimeZone;
 
 @Component
-public class Starter{
+public class Starter {
 
-    private static final Logger log = LoggerFactory.getLogger(ScheduledWeather.class);
-    WeatherService weatherService;
-    CurrentWeather currentWeather;
-    LocalDateTime sunrise, sunset;
-
-    @Autowired
-    public Starter(WeatherService weatherService) {
-        this.weatherService = weatherService;
-        sunrise = null;
-        sunset = null;
-    }
-
-    @EventListener(ContextRefreshedEvent.class)
-    public void run() {
-        currentWeather = weatherService.getCurrentWeather();
-        long sunriseUNIX = currentWeather.getSys().getSunrise();
-        long sunsetUNIX = currentWeather.getSys().getSunset();
-
-        sunrise = LocalDateTime
-                .ofInstant(Instant.ofEpochMilli(sunriseUNIX), TimeZone.getDefault().toZoneId());
-        sunset = LocalDateTime
-                .ofInstant(Instant.ofEpochMilli(sunsetUNIX), TimeZone.getDefault().toZoneId());
-
-        String firstWeatherLog = String.format("%s %s",sunrise,sunset);
-        log.info(firstWeatherLog);
-
-    }
 }
